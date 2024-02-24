10d871749ba9b9525ecbd5d6693977756d6a014a
manoj <manojvarmapolakonda@gmail.com>
2024-02-24 21:20:26 +0530

    this is firstfile

---
+++ src/test/java/githubtest/TestGrouping1.java
@@ -0,0 +1,23 @@
+package githubtest;
+
+import org.testng.annotations.Test;
+
+public class TestGrouping1 {
+
+	@Test
+	public void test_case001() {
+		System.out.println("this is test case 001");
+	}
+	
+	
+	@Test
+	public void test_case002() {
+		System.out.println("this is test case 002");
+	}
+	
+	@Test
+	public void test_case003() {
+		System.out.println("this is test case 003");
+	}
+}
+
---
+++ src/test/java/githubtest/TestGrouping2.java
@@ -0,0 +1,22 @@
+package githubtest;
+
+import org.testng.annotations.Test;
+
+public class TestGrouping2 {
+	@Test
+	public void test_case004() {
+		System.out.println("this is test case 004");
+	}
+	
+	
+	@Test
+	public void test_case005() {
+		System.out.println("this is test case 005");
+	}
+	
+	@Test
+	public void test_case006() {
+		System.out.println("this is test case 006");
+	}
+
+}
